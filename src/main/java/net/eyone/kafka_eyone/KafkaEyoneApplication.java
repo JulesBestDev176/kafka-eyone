7f2a3cbbe20c1f758546d09bbcf088e5add2be47
EduPortal Team <team@eduportal.com>
2025-09-15 12:38:27 +0000

    first commit

---
+++ src/main/java/net/eyone/kafka_eyone/KafkaEyoneApplication.java
@@ -0,0 +1,13 @@
+package net.eyone.kafka_eyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEyoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/eyone/kafka_eyone/KafkaEyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eyone.kafka_eyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEyoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
