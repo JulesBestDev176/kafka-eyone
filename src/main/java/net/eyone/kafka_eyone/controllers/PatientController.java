eaafd31a4a84cf74e6a691b3e208eca7c229a7f3
EduPortal Team <team@eduportal.com>
2025-09-23 10:58:55 +0000

    Refactor Patient creation logic to use PatientMapper for improved code clarity

--- src/main/java/net/eyone/kafka_eyone/controllers/PatientController.java
+++ src/main/java/net/eyone/kafka_eyone/controllers/PatientController.java
@@ -2,32 +2,25 @@ package net.eyone.kafka_eyone.controllers;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.eyone.kafka_eyone.dtos.PatientRequest;
-import net.eyone.kafka_eyone.dtos.PatientResponse;
 import net.eyone.kafka_eyone.models.Patient;
 import net.eyone.kafka_eyone.services.PatientProducerService;
+import net.eyone.kafka_eyone.mappers.PatientMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/patients")
 @RequiredArgsConstructor
 public class PatientController {
 
     private final PatientProducerService patientProducerService;
+    private final PatientMapper patientMapper;
+    
     @PostMapping
     public ResponseEntity<Void> create(@Valid @RequestBody PatientRequest request) {
-        Patient patient = Patient.builder()
-                .id(UUID.randomUUID())
-                .prenom(request.getPrenom())
-                .nom(request.getNom())
-                .numero(request.getNumero())
-                .sexe(request.getSexe())
-                .age(request.getAge())
-                .build();
-
+        Patient patient = patientMapper.toPatient(request);
         patientProducerService.publish(patient);
-
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/net/eyone/kafka_eyone/mappers/PatientMapper.java
+++ src/main/java/net/eyone/kafka_eyone/mappers/PatientMapper.java
@@ -1,13 +1,27 @@
 package net.eyone.kafka_eyone.mappers;
 
+import net.eyone.kafka_eyone.dtos.PatientRequest;
 import net.eyone.kafka_eyone.dtos.PatientResponse;
+import net.eyone.kafka_eyone.models.Patient;
 import net.eyone.kafka_eyone.models.PatientDocument;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Component
 public class PatientMapper {
+
+    public Patient toPatient(PatientRequest request) {
+        return Patient.builder()
+                .id(UUID.randomUUID())
+                .prenom(request.getPrenom())
+                .nom(request.getNom())
+                .numero(request.getNumero())
+                .sexe(request.getSexe())
+                .age(request.getAge())
+                .build();
+    }
 
     public PatientDocument toDocument(PatientResponse patientResponse) {
         return PatientDocument.builder()
