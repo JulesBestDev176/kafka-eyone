cd6a151499f4342c20e9a25bcac9f32cb0799ae8
EduPortal Team <team@eduportal.com>
2025-09-23 10:49:36 +0000

    Update application properties prefix from 'app' to 'application'

--- src/main/java/net/eyone/kafka_eyone/config/AppProperties.java
+++ src/main/java/net/eyone/kafka_eyone/config/AppProperties.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "app")
+@ConfigurationProperties(prefix = "application")
 public class AppProperties {
     private Kafka kafka = new Kafka();
     private Webhook webhook = new Webhook();
